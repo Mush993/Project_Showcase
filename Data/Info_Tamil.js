3d1ecf0a15507316f07caf45280467775283184f
Mush993 <86290958+Mush993@users.noreply.github.com>
2023-08-22 09:32:35 -0500

    Fish mesuem
    Mushrif

---
+++ Data/Info_Tamil.js
@@ -0,0 +1,237 @@
+﻿[
+    {
+        "Id": "0",
+        "Title": "1-Mal-Puluttaa.png",
+        "Imagesequence": "1",
+        "Index": "Fish.391.{index}.png",
+        "ImagesequenceQty": "146",
+        "Description": "1-Mal-Puluttaa.png",
+        "DescriptionMap": "1-Mal-Puluttaa.png",
+        "StilImg":""
+    },
+    {
+        "Id": "1",
+        "Title": "2-Pulli-Ahiraawaa.png",
+        "Imagesequence": "2",
+        "Index": "Fish.392.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "2-Pulli-Ahiraawaa.png",
+        "DescriptionMap": "2-Pulli-Ahiraawaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "2",
+        "Title": "3-Pathirana-Saalayaa.png",
+        "Imagesequence": "3",
+        "Index": "Fish.393.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "3-Pathirana-Saalayaa.png",
+        "DescriptionMap": "3-Pathirana-Saalayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "3",
+        "Title": "4-Loku-Gadayaa.png",
+        "Imagesequence": "4",
+        "Index": "Fish.394.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "4-Loku-Gadayaa.png",
+        "DescriptionMap": "4-Loku-Gadayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "4",
+        "Title": "5-Thambalayaa.png",
+        "Imagesequence": "5",
+        "Index": "Fish.395.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "5-Thambalayaa.png",
+        "DescriptionMap": "5-Thambalayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "5",
+        "Title": "6-Dumbara-Kara-Andayaa.png",
+        "Imagesequence": "6",
+        "Index": "Fish.396.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "6-Dumbara-Kara-Andayaa.png",
+        "DescriptionMap": "6-Dumbara-Kara-Andayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "6",
+        "Title": "7-Waruna-Kara-Andayaa.png",
+        "Imagesequence": "7",
+        "Index": "Fish.397.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "7-Waruna-Kara-Andayaa.png",
+        "DescriptionMap": "7-Waruna-Kara-Andayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "7",
+        "Title": "8-Rakwaana-Dandiyaa.png",
+        "Imagesequence": "8",
+        "Index": "Fish.398.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "8-Rakwaana-Dandiyaa.png",
+        "DescriptionMap": "8-Rakwaana-Dandiyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "8",
+        "Title": "9-Belihuloya-Dandiyaa.png",
+        "Imagesequence": "9",
+        "Index": "Fish.399.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "9-Belihuloya-Dandiyaa.png",
+        "DescriptionMap": "9-Belihuloya-Dandiyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "9",
+        "Title": "10-Kalu-Waral-Hal-Mal-Dandiyaa.png",
+        "Imagesequence": "10",
+        "Index": "Fish.400.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "10-Kalu-Waral-Hal-Mal-Dandiyaa.png",
+        "DescriptionMap": "10-Kalu-Waral-Hal-Mal-Dandiyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "10",
+        "Title": "11-Asoka-Pethyaa.png",
+        "Imagesequence": "11",
+        "Index": "Fish.401.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "11-Asoka-Pethyaa.png",
+        "DescriptionMap": "11-Asoka-Pethyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "11",
+        "Title": "12-Dumbara-Pethiyaa.png",
+        "Imagesequence": "12",
+        "Index": "Fish.403.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "12-Dumbara-Pethiyaa.png",
+        "DescriptionMap": "12-Dumbara-Pethiyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "12",
+        "Title": "13-Gal-Weligowwaa.png",
+        "Imagesequence": "13",
+        "Index": "Fish.404.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "13-Gal-Weligowwaa.png",
+        "DescriptionMap": "13-Gal-Weligowwaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "13",
+        "Title": "14-Sri-Lanka-Ankuttaa.png",
+        "Imagesequence": "14",
+        "Index": "Fish.415.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "14-Sri-Lanka-Ankuttaa.png",
+        "DescriptionMap": "14-SriLanka-Ankuttaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "14",
+        "Title": "15-Aaraa.png",
+        "Imagesequence": "15",
+        "Index": "Fish.416.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "15-Aaraa.png",
+        "DescriptionMap": "15-Aaraa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "15",
+        "Title": "16-Kaha-Waral-Depulliyaa.png",
+        "Imagesequence": "16",
+        "Index": "Fish.405.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "16-Kaha-Waral-Depulliyaa.png",
+        "DescriptionMap": "16-Kaha-Waral-Depulliyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "16",
+        "Title": "17-Bulath-Hapayaa.png",
+        "Imagesequence": "17",
+        "Index": "Fish.406.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "17-Bulath-Hapayaa.png",
+        "DescriptionMap": "17-Bulath-Hapayaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "17",
+        "Title": "18-Rathu-Waral-Depulliyaa.png",
+        "Imagesequence": "18",
+        "Index": "Fish.407.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "18-Rathu-Waral-Depulliyaa.png",
+        "DescriptionMap": "18-Rathu-Waral-Depulliyaa.png",
+        "StilImg": ""
+    },
+    {
+        "Id": "18",
+        "Title": "19-Mada-Pethiyaa.png",
+        "Imagesequence": "19",
+        "Index": "Fish.408.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "19-Mada-Pethiyaa.png",
+        "DescriptionMap": "19-Mada-Pethiyaa.png",
+        "StilImg": ""
+    }, {
+        "Id": "19",
+        "Title": "20-Rathu-Es-Katu-Pethiyaa.png",
+        "Imagesequence": "20",
+        "Index": "Fish.409.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "20-Rathu-Es-Katu-Pethiyaa.png",
+        "DescriptionMap": "20-Rathu-Es-Katu-Pethiyaa.png",
+        "StilImg": ""
+    }, {
+        "Id": "20",
+        "Title": "21-Lee-Thiththayaa.png",
+        "Imagesequence": "21",
+        "Index": "Fish.411.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "21-Lee-Thiththayaa.png",
+        "DescriptionMap": "21-Lee-Thiththayaa.png",
+        "StilImg": ""
+    }, {
+        "Id": "21",
+        "Title": "22-Wilpita-Dandiyaa.png",
+        "Imagesequence": "22",
+        "Index": "Fish.412.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "22-Wilpita-Dandiyaa.png",
+        "DescriptionMap": "22-Wilpita-Dandiyaa.png",
+        "StilImg": ""
+    }, {
+        "Id": "22",
+        "Title": "23-Halmal-Dandiyaa.png",
+        "Imagesequence": "23",
+        "Index": "Fish.413.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "23-Halmal-Dandiyaa.png",
+        "DescriptionMap": "23-Halmal-Dandiyaa.png",
+        "StilImg": ""
+    }, {
+        "Id": "23",
+        "Title": "24-Rathu-Thol-Weligowwaa.png",
+        "Imagesequence": "24",
+        "Index": "Fish.414.{index}.png",
+        "ImagesequenceQty": "143",
+        "Description": "24-Rathu-Thol-Weligowwaa.png",
+        "DescriptionMap": "24-Rathu-Thol-Weligowwaa.png",
+        "StilImg": ""
+    }
+]
